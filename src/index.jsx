0c78708688fdfeba6424f62c4a6a9a7a2974c6be
HowAboutRyze <lizhongwei157@163.com>
2020-07-24 00:40:01 +0800

    chore: vscode code-snippets

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class Index extends Component {
   render() {
-    return <h1>ryze index</h1>;
+    return (
+      <div className="page-index">
+        <h1>ryze index</h1>
+      </div>
+    );
   }
 }
