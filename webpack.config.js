ce73c2361bae97230279689001cfb3054e593681
HowAboutRyze <lizhongwei157@163.com>
2020-07-24 23:56:34 +0800

    deps: add html-webpack-alter-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
+const HtmlWebpackAlterPlugin = require('html-webpack-alter-plugin');
+const path = require('path');
+const projectConfig = path.join(process.cwd(), 'project.config.js');
+const projectData = require(projectConfig);
+
 module.exports = {
   target:'web', 
   framework: 'react',
   entry: {
     index: 'src/app.js'
   },
+  plugins: [
+    new HtmlWebpackAlterPlugin({
+      position: 'head',
+      content: [{
+        type: 'script',
+        data: `window.__PROJECT_STATE__=${JSON.stringify(projectData)}`
+      }]
+    })
+  ]
 };
